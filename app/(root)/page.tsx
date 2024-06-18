bf4dc7df3b8d72ca37300a45e8fd3e2479df500f
s3xmaxman <badside3919@gmail.com>
2024-06-18 14:08:36 +0900

    update 3 files and delete 1 file

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,26 @@
+import PodcastCard from "@/components/PodcastCard";
 import { Button } from "@/components/ui/button";
+import { podcastData } from "@/constants";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="mt-9 flex flex-col gap-9">
+    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <div className="podcast_grid">
+          {podcastData.map(({ id, title, description, imgURL }) => {
+            return (
+              <PodcastCard
+                key={id}
+                imgUrl={imgURL}
+                title={title}
+                description={description}
+                podcastId={id}
+              />
+            );
+          })}
+        </div>
       </section>
     </div>
   );
---
+++ components/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return (
+    <section className="right_sidebar text-white-1">
+      <h1>Right Sidebar</h1>
+    </section>
+  );
+};
+
+export default RightSidebar;
