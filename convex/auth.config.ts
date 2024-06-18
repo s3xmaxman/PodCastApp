59870bef7185bb0d0fa22222e88a6bd3cd0e2819
s3xmaxman <badside3919@gmail.com>
2024-06-18 17:04:38 +0900

    build(deps): update package-lock.json, package.json and ConvexClerkProvider.tsx

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://mutual-dassie-54.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
--- providers/ConvexClerkProvider.tsx
+++ providers/ConvexClerkProvider.tsx
@@ -1,13 +1,35 @@
 "use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL as string
+);
 
-export default function ConvexClientProvider({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
-}
+const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
+  <ClerkProvider
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+    appearance={{
+      layout: {
+        socialButtonsVariant: "iconButton",
+        logoImageUrl: "/icons/auth-logo.svg",
+      },
+      variables: {
+        colorBackground: "#15171c",
+        colorPrimary: "",
+        colorText: "white",
+        colorInputBackground: "#1b1f29",
+        colorInputText: "white",
+      },
+    }}
+  >
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  </ClerkProvider>
+);
+
+export default ConvexClerkProvider;
