52c1874170203e9fd9a2d031b31dcd62fb7c0278
s3xmaxman <badside3919@gmail.com>
2024-06-18 16:48:23 +0900

    update 5 files

---
+++ convex/users.ts
@@ -0,0 +1,19 @@
+import { v } from "convex/values";
+import { internalMutation } from "./_generated/server";
+
+export const createUser = internalMutation({
+  args: {
+    clerkId: v.string(),
+    email: v.string(),
+    name: v.string(),
+    imageUrl: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("users", {
+      clerkId: args.clerkId,
+      email: args.email,
+      name: args.name,
+      imageUrl: args.imageUrl,
+    });
+  },
+});
