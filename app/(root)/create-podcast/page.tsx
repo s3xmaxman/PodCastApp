22a4dd5171c4044b0bf113ffd35c1bdc88daf1b4
s3xmaxman <badside3919@gmail.com>
2024-06-18 19:55:36 +0900

    build(deps): update page.tsx, package-lock.json and package.json

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import React from "react";
+import { z } from "zod";
+
+const formSchema = z.object({
+  podcastTitle: z.string().min(2),
+  podcastDescription: z.string().min(2),
+});
 
 const CreatePodcast = () => {
-  return <div>CreatePodcast</div>;
+  return (
+    <div>
+      <h1>CreatePodcast</h1>
+    </div>
+  );
 };
 
 export default CreatePodcast;
