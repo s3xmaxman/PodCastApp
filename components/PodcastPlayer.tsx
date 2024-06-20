93704317089840624b94934b878561939abaed20
s3xmaxman <badside3919@gmail.com>
2024-06-20 21:22:42 +0900

    update 7 files

---
+++ components/PodcastPlayer.tsx
@@ -0,0 +1,212 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+
+import { formatTime } from "@/lib/formatTime";
+import { cn } from "@/lib/utils";
+import { useAudio } from "@/providers/AudioProvider";
+
+import { Progress } from "./ui/progress";
+
+/**
+ * PodcastPlayer コンポーネント
+ *
+ * このコンポーネントは、ポッドキャストプレーヤーの UI をレンダリングします。
+ * `useAudio` プロバイダーを使用して、現在のオーディオデータにアクセスします。
+ */
+const PodcastPlayer = () => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [isMuted, setIsMuted] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const { audio } = useAudio();
+
+  /**
+   * オーディオプレーヤーの再生/一時停止状態を切り替えます。
+   */
+  const togglePlayPause = () => {
+    if (audioRef.current?.paused) {
+      audioRef.current?.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.current?.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  /**
+   * オーディオプレーヤーのミュート状態を切り替えます。
+   */
+  const toggleMute = () => {
+    if (audioRef.current) {
+      audioRef.current.muted = !isMuted;
+      setIsMuted((prev) => !prev);
+    }
+  };
+
+  /**
+   * オーディオを 5 秒間早送りします。
+   */
+  const forward = () => {
+    if (
+      audioRef.current &&
+      audioRef.current.currentTime &&
+      audioRef.current.duration &&
+      audioRef.current.currentTime + 5 < audioRef.current.duration
+    ) {
+      audioRef.current.currentTime += 5;
+    }
+  };
+
+  /**
+   * オーディオを 5 秒間巻き戻します。
+   */
+  const rewind = () => {
+    if (audioRef.current && audioRef.current.currentTime - 5 > 0) {
+      audioRef.current.currentTime -= 5;
+    } else if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+    }
+  };
+
+  /**
+   * オーディオプレーヤーの現在時刻を更新します。
+   */
+  useEffect(() => {
+    const updateCurrentTime = () => {
+      if (audioRef.current) {
+        setCurrentTime(audioRef.current.currentTime);
+      }
+    };
+
+    const audioElement = audioRef.current;
+    if (audioElement) {
+      audioElement.addEventListener("timeupdate", updateCurrentTime);
+
+      return () => {
+        audioElement.removeEventListener("timeupdate", updateCurrentTime);
+      };
+    }
+  }, []);
+
+  /**
+   * `audioUrl` が利用可能になったらオーディオを再生します。
+   */
+  useEffect(() => {
+    const audioElement = audioRef.current;
+    if (audio?.audioUrl) {
+      if (audioElement) {
+        audioElement.play().then(() => {
+          setIsPlaying(true);
+        });
+      }
+    } else {
+      audioElement?.pause();
+      setIsPlaying(true);
+    }
+  }, [audio]);
+
+  /**
+   * メタデータが読み込まれたら、オーディオの期間を設定します。
+   */
+  const handleLoadedMetadata = () => {
+    if (audioRef.current) {
+      setDuration(audioRef.current.duration);
+    }
+  };
+
+  /**
+   * オーディオが終了したら、再生状態を false に設定します。
+   */
+  const handleAudioEnded = () => {
+    setIsPlaying(false);
+  };
+
+  return (
+    <div
+      className={cn("sticky bottom-0 left-0 flex size-full flex-col", {
+        hidden: !audio?.audioUrl || audio?.audioUrl === "",
+      })}
+    >
+      <Progress
+        value={(currentTime / duration) * 100}
+        className="w-full"
+        max={duration}
+      />
+      <section className="glassmorphism-black flex h-[112px] w-full items-center justify-between px-4 max-md:justify-center max-md:gap-5 md:px-12">
+        <audio
+          ref={audioRef}
+          src={audio?.audioUrl}
+          className="hidden"
+          onLoadedMetadata={handleLoadedMetadata}
+          onEnded={handleAudioEnded}
+        />
+        <div className="flex items-center gap-4 max-md:hidden">
+          <Link href={`/podcast/${audio?.podcastId}`}>
+            <Image
+              src={audio?.imageUrl! || "/images/player1.png"}
+              width={64}
+              height={64}
+              alt="player1"
+              className="aspect-square rounded-xl"
+            />
+          </Link>
+          <div className="flex w-[160px] flex-col">
+            <h2 className="text-14 truncate font-semibold text-white-1">
+              {audio?.title}
+            </h2>
+            <p className="text-12 font-normal text-white-2">{audio?.author}</p>
+          </div>
+        </div>
+        <div className="flex-center cursor-pointer gap-3 md:gap-6">
+          <div className="flex items-center gap-1.5">
+            <Image
+              src={"/icons/reverse.svg"}
+              width={24}
+              height={24}
+              alt="巻き戻し"
+              onClick={rewind}
+            />
+            <h2 className="text-12 font-bold text-white-4">-5</h2>
+          </div>
+          <Image
+            src={isPlaying ? "/icons/Pause.svg" : "/icons/Play.svg"}
+            width={30}
+            height={30}
+            alt="再生"
+            onClick={togglePlayPause}
+          />
+          <div className="flex items-center gap-1.5">
+            <h2 className="text-12 font-bold text-white-4">+5</h2>
+            <Image
+              src={"/icons/forward.svg"}
+              width={24}
+              height={24}
+              alt="早送り"
+              onClick={forward}
+            />
+          </div>
+        </div>
+        <div className="flex items-center gap-6">
+          <h2 className="text-16 font-normal text-white-2 max-md:hidden">
+            {formatTime(duration)}
+          </h2>
+          <div className="flex w-full gap-2">
+            <Image
+              src={isMuted ? "/icons/unmute.svg" : "/icons/mute.svg"}
+              width={24}
+              height={24}
+              alt="ミュート/ミュート解除"
+              onClick={toggleMute}
+              className="cursor-pointer"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default PodcastPlayer;
