cc86825d1b421861d010cd8ea2e36bf6656bcb04
s3xmaxman <badside3919@gmail.com>
2024-06-20 17:48:59 +0900

    update 5 files

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { SignedIn, UserButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Header from "./Header";
+import Carousel from "./Carousel";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const RightSidebar = () => {
   const { user } = useUser();
+  const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
   return (
     <section className="right_sidebar text-white-1">
       <SignedIn>
@@ -14,9 +20,19 @@ const RightSidebar = () => {
             <h1 className="text-16 truncate font-semibold text-white-1">
               {user?.firstName} {user?.lastName}
             </h1>
+            <Image
+              src="/icons/right-arrow.svg"
+              width={24}
+              height={24}
+              alt="right arrow icon"
+            />
           </div>
         </Link>
       </SignedIn>
+      <section>
+        <Header headerTitle="Fans Like You" />
+        {/* <Carousel fansLikeDetail={topPodcasters} /> */}
+      </section>
     </section>
   );
 };
